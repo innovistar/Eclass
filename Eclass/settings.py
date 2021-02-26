5d7fd37bb739a516885512c54087bf40c3e95c12
innovistar <45834254+innovistar@users.noreply.github.com>
2021-02-26 12:48:27 +0300

    Update settings.py

--- Eclass/settings.py
+++ Eclass/settings.py
@@ -37,11 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'courses',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -69,6 +72,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Eclass.wsgi.application'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Database
