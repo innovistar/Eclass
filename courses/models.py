4fbbd8af033e6288cc354e2d96644dc68924937b
innovistar <45834254+innovistar@users.noreply.github.com>
2021-02-26 12:32:51 +0300

    Update models.py

--- courses/models.py
+++ courses/models.py
@@ -30,3 +30,49 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.topic
+
+class Notification(models.Model):
+	title = models.CharField(max_length=200, null=True)
+	notice = models.CharField(max_length=2000)
+	
+	def __str__(self):
+		return self.notice
+	
+class Books(models.Model):
+	image = models.ImageField(null=True, blank=True)
+	title = models.CharField(max_length=200)
+	note = models.CharField(max_length=10000, null=True, blank=True)
+	image_url = models.CharField(max_length=2000, null=True, blank=True)
+	url = models.CharField(max_length=200)
+	
+	def __str__(self):
+		return self.title
+
+	@property
+	def imageURL(self):
+		try:
+			url = self.image.url
+		except :
+			url = ''
+		return url
+	
+class Events(models.Model):
+	event = models.ImageField(null=True, blank=True)
+	title = models.CharField(max_length=200)
+	note = models.CharField(max_length=10000, null=True, blank=True)
+	url = models.CharField(max_length=200)
+	image_url = models.CharField(max_length=2000, null=True, blank=True)
+	#videourl = EmbedVideoField(null=True, blank=True)
+	
+	
+	def __str__(self):
+		return self.url
+
+
+	@property
+	def imageURL(self):
+		try:
+			url = self.image.url
+		except :
+			url = ''
+		return url
