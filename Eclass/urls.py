5858c2ff66afd5a4d7c22fd7c1895b9ef344a645
innovistar <45834254+innovistar@users.noreply.github.com>
2021-02-26 12:51:45 +0300

    Update urls.py

--- Eclass/urls.py
+++ Eclass/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -23,6 +23,7 @@ from courses.views import course
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include("courses.api.urls"), name="api"),
     path('', course, name="home")
 ]
 
